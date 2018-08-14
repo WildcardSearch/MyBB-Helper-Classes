af2a012e6f82cd38707268ddb1018f05d26e359a
Mark Vincent <admin@rantcentralforums.com>
2018-08-13 22:14:11 -0500

    Installer 1.2.3 -> 1.3

--- src/WildcardPluginInstaller010300.php
+++ src/WildcardPluginInstaller010300.php
@@ -9,12 +9,12 @@
  *
  */
 
-class WildcardPluginInstaller010203 implements WildcardPluginInstallerInterface010000
+class WildcardPluginInstaller010300 implements WildcardPluginInstallerInterface010000
 {
 	/**
 	 * @const version
 	 */
-	const VERSION = '1.2.3';
+	const VERSION = '1.3';
 
 	/**
 	 * @var object a copy of the MyBB db object
@@ -147,6 +147,12 @@ class WildcardPluginInstaller010203 implements WildcardPluginInstallerInterface0
 			case 'columns':
 			case 'images':
 				break;
+			case 'tables':
+				if ($db->engine == 'pgsql') {
+					$this->tables = $tables['pgsql'];
+				} else {
+					unset($this->tables['pgsql']);
+				}
 			default:
 				$singular = substr($key, 0, strlen($key) - 1);
 				$property = "{$singular}Names";
@@ -216,8 +222,11 @@ class WildcardPluginInstaller010203 implements WildcardPluginInstallerInterface0
 
 		// create the table if it doesn't already exist
 		if (!$this->tableExists($table)) {
-			$table =  TABLE_PREFIX . $table;
-			$this->db->write_query("CREATE TABLE {$table} ({$columnList}) ENGINE={$this->db->table_type}{$collation};");
+			$queryString = "CREATE TABLE {$this->db->table_prefix}{$table} ({$columnList});";
+			if ($this->db->engine != 'pgsql') {
+				$queryString .= "ENGINE={$this->db->table_type}{$collation}";
+			}
+			$this->db->write_query($queryString);
 		}
 	}
 
@@ -256,8 +265,9 @@ class WildcardPluginInstaller010203 implements WildcardPluginInstallerInterface0
 			return;
 		}
 
-		$dropList = implode(', ' . TABLE_PREFIX, $this->tableNames);
-		$this->db->drop_table($dropList);
+		foreach ($this->tableNames as $table) {
+			$this->db->drop_table($table);
+		}
 	}
 
 	/**
@@ -770,14 +780,23 @@ class WildcardPluginInstaller010203 implements WildcardPluginInstallerInterface0
 	{
 		global $config;
 
-		$query = $this->db->write_query("
-			SHOW TABLES
-			FROM `{$config['database']['database']}`
-		");
+		// PostgreSQL requires a little more work to grab the table names
+		if ($this->db->engine == 'pgsql') {
+			$tableArray = $this->db->list_tables($config['database']['database'], $this->db->table_prefix);
 
-		$tableList = array();
-		while ($row = $this->db->fetch_array($query)) {
-			$tableList[array_pop($row)] = 1;
+			foreach ($tableArray as $table) {
+				$tableList[$table] = 1;
+			}
+		} else {
+			$query = $this->db->write_query("
+				SHOW TABLES
+				FROM `{$config['database']['database']}`
+			");
+
+			$tableList = array();
+			while ($row = $this->db->fetch_array($query)) {
+				$tableList[array_pop($row)] = 1;
+			}
 		}
 		return $tableList;
 	}
